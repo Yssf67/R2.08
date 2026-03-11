936a58678b4ff825c6f49c3a962d983202bf7a39
Yssf67 <yssf.a672@gmail.com>
2026-03-11 15:57:26 +0100

    Maj 11/03

--- TD2 Ex1.py
+++ TD2 Ex1.py
@@ -11,5 +11,15 @@ while True:
     try :
         index = int(input("Veuillez donner un index :"))
         print(liste[index])
+
+        a = int(input("Donnez un premier nombre : "))
+        b = int(input("Donnez un second nombre : "))
+        print("Résultat :", a / b)
     except IndexError:
         print(f"ERREUR : L'index {index} est en dehors de la liste ([{-len(liste)}; {len(liste)-1 }])")
+
+    except ValueError:
+        print("ERREUR : La valeur saisie doit être un nombre ...")
+
+    except ZeroDivisionError:
+        print("ERREUR: La division par 0 est impossible")
